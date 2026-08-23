f1b0f01c06e44058f731078146f9e5795b468778
prashantk153 <prashantk.ee.07@gmail.com>
2026-08-23 19:50:05 +0530

    assignment 1 answered

---
+++ assignment1.py
@@ -0,0 +1,7 @@
+# question- print your name 10 times using the * and on every new line
+
+print("Prashant Kashyap " * 10)
+
+# In above code to make it in every new line we have to use \n
+
+print(("Prashant Kashyap " + "\n")*10)
