bc44dfa0b18e412c8f8f67c4964a35f8084c8514
prashantk153 <prashantk.ee.07@gmail.com>
2026-08-24 00:54:50 +0530

    🐍 Python Fundamentals — Variables, Keywords, Constants, Identifiers & input()

--- Day 1/assignment1.py
+++ Day 1/assignment1.py
@@ -5,3 +5,11 @@ print("Prashant Kashyap " * 10)
 # In above code to make it in every new line we have to use \n
 
 print(("Prashant Kashyap " + "\n")*10)
+
+# id() gives the momeory location where the variable points.
+
+a = 10
+print(id(a))
+
+
+
